d2817ded566cb24361e40e2ec837a7b15c3819d9
DriesVerstraeten <driesverstraeten@wlan-145-94-223-222.wlan.tudelft.nl>
2017-05-30 14:36:26 +0200

    Some parameters added

---
+++ Parameters.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Author: Dries
+"""
+
+# THIS FILE IS STRICTLY AND ONLY FOR PARAMETERS - NO CALCULATIONS!
+
+Lt =  5.7 #m - Tail length
+S = 16.05 #m2 - Wing surface area
+Lf = 9.04 #m - fuselage length
