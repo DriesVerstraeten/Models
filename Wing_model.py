2b2cf7a8ae7bc9a829a923515ca3345a7460c1f8
DriesVerstraeten <driesverstraeten@wlan-145-94-223-222.wlan.tudelft.nl>
2017-05-30 15:10:00 +0200

    wing model file created

---
+++ Wing_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 15:08:10 2017
+
+@author: driesverstraeten
+"""
+
+#Calculations for the WING MODEL
+
+import Parameters.py as p
+
+print p.S
