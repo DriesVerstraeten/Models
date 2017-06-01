abf698aadf688fe4e90660404665f03d45b93359
Lootuscobra <30v22v50s@gmail.com>
2017-06-01 14:45:32 +0200

    Cost

--- Material_properties.py
+++ Material_properties.py
@@ -18,6 +18,7 @@ Fty = np.zeros(16)
 Ftu = np.zeros(16)
 Fsu = np.zeros(16)
 Fbru = np.zeros(16)
+Cost = np.zeros(16)
 
 #2024-T3
 rho[0] = 0.1 #density lb/in^3
@@ -28,7 +29,7 @@ Fty[0] = 47. #yield tensile ksi
 Ftu[0] = 64. #ultimate tensile ksi
 Fsu[0] = 39. #ultimate shear ksi
 Fbru[0] = 104. # ultimate bearing e/D=1.5 ksi
-#add cost from somewhere
+Cost[0] = 2.5 #dollar per kg
 
 #2024-T4
 rho[1] = 0.1 #density lb/in^3
@@ -39,6 +40,7 @@ Fty[1] = 40. #yield tensile ksi
 Ftu[1] = 62. #ultimate tensile ksi
 Fsu[1] = 37. #ultimate shear ksi
 Fbru[1] = 93. # ultimate bearing e/D=1.5 ksi
+Cost[1] = 2.5 #dollar per kg
 
 #6061-T6
 rho[2] = 0.098 #density lb/in^3
@@ -49,6 +51,7 @@ Fty[2] = 36. #yield tensile ksi
 Ftu[2] = 42. #ultimate tensile ksi
 Fsu[2] = 36. #ultimate shear ksi
 Fbru[2] = 67. # ultimate bearing e/D=1.5 ksi
+Cost[0] = 2.5 #dollar per kg
 
 #7075-T6
 rho[3] = 0.101 #density lb/in^3
@@ -59,6 +62,7 @@ Fty[3] = 70. #yield tensile ksi
 Ftu[3] = 78. #ultimate tensile ksi
 Fsu[3] = 47. #ultimate shear ksi
 Fbru[3] = 121. # ultimate bearing e/D=1.5 ksi
+Cost[0] = 2.5 #dollar per kg
 
 #AISI 1025
 rho[4] = 0.284 #density lb/in^3
@@ -69,6 +73,7 @@ Fty[4] = 36. #yield tensile ksi
 Ftu[4] = 55. #ultimate tensile ksi
 Fsu[4] = 35. #ultimate shear ksi
 Fbru[4] = 90. # ultimate bearing e/D=2 ksi
+Cost = 0.5 #dollar per kg
 
 #AISI 4130 (t<0.118inch)
 rho[5] = 0.283 #density lb/in^3
@@ -79,6 +84,7 @@ Fty[5] = 75. #yield tensile ksi
 Ftu[5] = 95. #ultimate tensile ksi
 Fsu[5] = 57. #ultimate shear ksi
 Fbru[5] = 200. # ultimate bearing e/D=2 ksi
+Cost = 0.8 #dollar per kg
 
 #AISI 4130 (t>0.118inch)
 rho[6] = 0.283 #density lb/in^3
@@ -89,6 +95,7 @@ Fty[6] = 70. #yield tensile ksi
 Ftu[6] = 90. #ultimate tensile ksi
 Fsu[6] = 54. #ultimate shear ksi
 Fbru[6] = 190. # ultimate bearing e/D=2 ksi
+Cost = 0.8 #dollar per kg
 
 #AISI 4130 (t<0.118inch tempered and quenched)
 rho[7] = 0.283 #density lb/in^3
@@ -99,6 +106,7 @@ Fty[7] = 100. #yield tensile ksi
 Ftu[7] = 125. #ultimate tensile ksi
 Fsu[7] = 75. #ultimate shear ksi
 Fbru[7] = 146. # ultimate bearing e/D=1.5 ksi
+Cost = 0.8 #dollar per kg
 
 #AISI 4340
 rho[8] = 0.283 #density lb/in^3
@@ -109,6 +117,7 @@ Fty[8] = 217. #yield tensile ksi
 Ftu[8] = 260. #ultimate tensile ksi
 Fsu[8] = 156. #ultimate shear ksi
 Fbru[8] = 347. # ultimate bearing e/D=1.5 ksi
+Cost = 1.5 #dollar per kg
 
 #300M
 rho[9] = 0.283 #density lb/in^3
@@ -119,6 +128,7 @@ Fty[9] = 220. #yield tensile ksi
 Ftu[9] = 270. #ultimate tensile ksi
 Fsu[9] = 162. #ultimate shear ksi
 Fbru[9] = 414. # ultimate bearing e/D=1.5 ksi
+Cost = 6.1 #dollar per kg
 
 #E-glass
 rho[10] = 0.094 #density lb/in^3
@@ -129,6 +139,7 @@ Fty[10] = 27. #yield tensile ksi
 Ftu[10] = 27. #ultimate tensile ksi
 Fsu[10] = 0. #ultimate shear ksi
 Fbru[10] = Ftu[10] # ultimate bearing e/D=1.5 ksi
+Cost = 12. #dollar per kg
 
 #S-glass
 rho[11] = 0.092 #density lb/in^3
@@ -139,6 +150,7 @@ Fty[11] = 90. #yield tensile ksi
 Ftu[11] = 90. #ultimate tensile ksi
 Fsu[11] = 0. #ultimate shear ksi
 Fbru[11] = Ftu[11] # ultimate bearing e/D=1.5 ksi
+Cost = 30. #dollar per kg
 
 #High-modulus carbon
 rho[12] = 0.072 #density lb/in^3
@@ -149,36 +161,29 @@ Fty[12] = 190. #yield tensile ksi
 Ftu[12] = 190. #ultimate tensile ksi
 Fsu[12] = 0. #ultimate shear ksi
 Fbru[12] = Ftu[12] # ultimate bearing e/D=1.5 ksi
-
-#High-modulus carbon
-rho[13] = 0.072 #density lb/in^3
-E[13] = 53.*1000 #tensile modulus ksi
-G[13] = 2.7 * 1000 #shear modulus ksi
-mu[13] = 0.2 #poisson
-Fty[13] = 190. #yield tensile ksi
-Ftu[13] = 190. #ultimate tensile ksi
+Cost = 600. #dollar per kg
+    
+#High-strength carbon
+rho[13] = 0.065 #density lb/in^3
+E[13] = 35.*1000 #tensile modulus ksi
+G[13] = 3.6 * 1000 #shear modulus ksi
+mu[13] = 0.3 #poisson
+Fty[13] = 320. #yield tensile ksi
+Ftu[13] = 320. #ultimate tensile ksi
 Fsu[13] = 0. #ultimate shear ksi
 Fbru[13] = Ftu[13] # ultimate bearing e/D=1.5 ksi
-
-#High-strength carbon
-rho[14] = 0.065 #density lb/in^3
-E[14] = 35.*1000 #tensile modulus ksi
-G[14] = 3.6 * 1000 #shear modulus ksi
-mu[14] = 0.3 #poisson
-Fty[14] = 320. #yield tensile ksi
-Ftu[14] = 320. #ultimate tensile ksi
-Fsu[14] = 0. #ultimate shear ksi
-Fbru[14] = Ftu[14] # ultimate bearing e/D=1.5 ksi
+Cost = 129. #dollar per kg
 
 #Aramid
-rho[15] = 0.052 #density lb/in^3
-E[15] = 18.*1000 #tensile modulus ksi
-G[15] = 4. * 1000 #shear modulus ksi
-mu[15] = 0.36 #poisson
-Fty[15] = 40. #yield tensile ksi
-Ftu[15] = 40. #ultimate tensile ksi
-Fsu[15] = 0. #ultimate shear ksi
-Fbru[15] = Ftu[15] # ultimate bearing e/D=1.5 ksi
+rho[14] = 0.052 #density lb/in^3
+E[14] = 18.*1000 #tensile modulus ksi
+G[14] = 4. * 1000 #shear modulus ksi
+mu[14] = 0.36 #poisson
+Fty[14] = 40. #yield tensile ksi
+Ftu[14] = 40. #ultimate tensile ksi
+Fsu[14] = 0. #ultimate shear ksi
+Fbru[14] = Ftu[14] # ultimate bearing e/D=1.5 ksi
+Cost = 93. #per kg
 
 #Convertion to normal units, lb/in^3 to kg/m^3 and ksi to Pa
 rho = rho * 27.7334934 * 1000
