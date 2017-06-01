0c08f8845823d8ae0e16e6e3769bb2eb0baa5803
DriesVerstraeten <driesverstraeten@wlan-145-94-220-146.wlan.tudelft.nl>
2017-06-01 14:55:33 +0200

    changes

--- Wing_model.py
+++ Wing_model.py
@@ -9,5 +9,18 @@ Created on Tue May 30 15:08:10 2017
 #Calculations for the WING MODEL
 
 import Parameters as p
+import numpy as np
 
-print p.S
+########## Chord length at different spanwise locations ##########
+dy = 0.01
+y = np.arange(0,p.b/2, dy) #spanwise section
+d_cLE = np.tan(p.theta_LE) * y #LE section "to be cut away from chord"
+d_cTE = np.tan(p.theta_TE) * y #TE section "to be cut away from chord"
+
+c = p.c_r - d_cLE - d_cTE #chord at each spanwise section
+
+
+########## Bending Diagram ##########
+
+dL = 1./2. * p.rho_0 * p.V_cruise**2 * dy * c * p.CL
+L = np.sum(dL)
