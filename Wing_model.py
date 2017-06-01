daa9b688299f98172bf50f4f8627fd1fdf47ea58
DriesVerstraeten <driesverstraeten@wlan-145-94-220-146.wlan.tudelft.nl>
2017-06-01 17:28:14 +0200

    last changes 01/06/2017

--- Wing_model.py
+++ Wing_model.py
@@ -10,10 +10,11 @@ Created on Tue May 30 15:08:10 2017
 
 import Parameters as p
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 ########## Chord length at different spanwise locations ##########
-dy = 0.01 #small spanwise section
+dy = 0.001 #small spanwise section
 y = np.arange(0,p.b/2., dy) #spanwise location of section
 d_cLE = np.tan(p.theta_LE) * y #LE section "to be cut away from chord"
 d_cTE = np.tan(p.theta_TE) * y #TE section "to be cut away from chord"
@@ -21,23 +22,22 @@ d_cTE = np.tan(p.theta_TE) * y #TE section "to be cut away from chord"
 c = p.c_r - d_cLE - d_cTE #chord at each spanwise section
 
 
-########## Shear at 9g ##########
+########## Bending at 9g ##########
 
 CL_9g = 9. * p.g * p.MTOW / (0.5 * p.rho_0 * p.V_cruise**2 * p.S) #lift coefficient at 9g
 
 dL_9g = CL_9g * 1./2. * p.rho_0 * p.V_cruise**2 * dy * c
 
 
-moment = dL_9g*y
-moment[0] = sum(dL_9g*y)
-
+moment_9g = dL_9g*y
+moment_9g[0] = sum(dL_9g*y)
+moment_9g_total = []
 
 for i in range(1,len(y)):
-    moment[i] = moment[i-1] - moment[i] 
-    print moment[i]
+    moment_9g[i] = moment_9g[i-1] - moment_9g[i] 
+    moment_9g_total.append(moment_9g[i])
 
+plt.plot(y[0:len(y)-1],moment_9g_total)
+plt.show
 
-
-
-dM_9g = dL_9g * y
 
