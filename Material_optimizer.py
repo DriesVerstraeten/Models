04c741aaa0f2580e121f9659ab29017357e4b3a3
Lootuscobra <30v22v50s@gmail.com>
2017-06-01 09:34:57 +0200

    Material optimiser master file

---
+++ Material_optimizer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 01 09:32:13 2017
+
+@author: 123
+"""
+#Here we put material properties, and by importing results from other files optimize according to weight-cost model. 
+#Need to find this model though. 
+#Inputs for each material: stiffness, cost per kg, density, other stuff if needed.
+
+#Material 1
+
+
+
+#Material 2
