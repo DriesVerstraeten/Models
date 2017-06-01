9a7bd701ae29b14b53b799447c69488bc3bb861c
Lootuscobra <30v22v50s@gmail.com>
2017-06-01 10:48:59 +0200

    Started adding materials

--- Material_optimizer.py
+++ Material_optimizer.py
@@ -8,8 +8,27 @@ Created on Thu Jun 01 09:32:13 2017
 #Need to find this model though. 
 #Inputs for each material: stiffness, cost per kg, density, other stuff if needed.
 
-#Material 1
+#2024-T3
+rho_1 = 0.1 #density lb/in^3
+E_1 = 10.5*10^3 #tensile modulus ksi
+G_1 = 4.0 * 10^3 #shear modulus ksi
+mu_1 = 0.33 #poisson
+Fty_1 = 47 #yield tensile ksi
+Ftu_1 = 64 #ultimate tensile ksi
+Fsu_1 = 39 #ultimate shear ksi
+Fbru_1 = 104 # ultimate bearing e/D=1.5 ksi
+#add cost from somewhere
+
+#2024-T4
+rho_2 = 0.1 #density lb/in^3
+E_2 = 10.5*10^3 #tensile modulus ksi
+G_2 = 4.0 * 10^3 #shear modulus ksi
+mu_2 = 0.33 #poisson
+Fty_2 = 40 #yield tensile ksi
+Ftu_2 = 62 #ultimate tensile ksi
+Fsu_2 = 37 #ultimate shear ksi
+Fbru_2 = 93 # ultimate bearing e/D=1.5 ksi
 
 
 
-#Material 2
+
