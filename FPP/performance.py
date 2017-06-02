832277caf82804a92a816cdb34c361ea8ca3e310
simondm96 <simon.de.meester@telenet.be>
2017-06-02 17:28:51 +0200

    Added files for propulsion and performance

---
+++ FPP/performance.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 02 17:26:23 2017
+
+@author: Simon
+
+This is the flight performance file
+"""
+
---
+++ FPP/propulsion.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 02 17:25:47 2017
+
+@author: Simon
+
+This is the propulsion file for the model
+
+Inputs from chosen engine: AI-450S or M250 B17F (for now) (450 shp)
+
+Other candidates: 
+    GE H80 (or H75)
+    Some diesel (Continental CD300 or so)
+    PT6A (medium version)
+    
+Using Blade Element Theory, I calculate the propellor chacteristics.
+    (mainly efficiency and power available)
+"""
+
+
