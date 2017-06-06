3cca546be3e8b8f12e73f1056b8b3072ca676e35
Lootuscobra <30v22v50s@gmail.com>
2017-06-06 15:26:43 +0200

    shear calculation

--- Structures/Tail_model.py
+++ Structures/Tail_model.py
@@ -14,6 +14,11 @@ import matplotlib.pyplot as plt
 import math
 from scipy import integrate
 
+#WINGBOX DIMENSIONS
+box_t = 0.0005 #m, thickness. same everywhere for now
+box_b = 0.25*p.cr_ht #m, width at rootapply scaling for other points. scales linearly with taper
+box_h = 0.1*p.cr_ht #m, height at root. apply scaling for other points. scales linearly with taper
+
 def y_tail(dy,i):
     y_tail = np.linspace(0,p.b_ht/2,dy)
     return y_tail[i]
@@ -43,7 +48,7 @@ def slope_c(dy):
 
 def tail_I_xx(dy,i):
     I_xx = np.zeros(dy)
-    I_xx = (p.cr_ht - y_tail_inv(dy,i) * slope_c(dy))**4 * (0.1*p.cr_ht)**3 * 0.25*p.cr_ht/12 - (p.cr_ht - y_tail_inv(dy,i) * slope_c(dy))**4 * (0.1*p.cr_ht - 0.0005)**3 * (0.25*p.cr_ht/12 - 0.0005)
+    I_xx = (p.cr_ht - y_tail_inv(dy,i) * slope_c(dy))**4 * (box_h)**3 * box_b/12 - (p.cr_ht - y_tail_inv(dy,i) * slope_c(dy))**4 * (box_h - box_t)**3 * (box_b - 0.0005)/12
     return I_xx
     #9G ULTIMATE STATIC LOAD HORIZONTAL TAIL
 
@@ -99,7 +104,55 @@ def tail_plots(dy,g):
     plt.show()
     return
 
-
+def tail_shear_stress(dy,dx,dz,i,g):
+    x_12 = np.linspace(0,((p.cr_ht - y_tail_inv(dy,i) * slope_c(dy)) * box_b/2),dx)
+    z_23 = np.linspace(((p.cr_ht - y_tail_inv(dy,i) * slope_c(dy)) * box_h)/2, -1*(p.cr_ht-y_tail_inv(dy,i)*slope_c(dy))*0.1*p.cr_ht/2, dz)
+    x_34 = np.linspace((p.cr_ht - y_tail_inv(dy,i) * slope_c(dy)) * box_b/2,0,dx)
+    q_12 = np.zeros(dx)
+    q_23 = np.zeros(dy)
+    q_34 = np.zeros(dx)
+    for j in range (0,dx):
+        q_12[j] = -tail_shear(dy,g,i) * box_t * z_23[0] * x_12[j] / (2*tail_I_xx(dy,i))
+    for j in range (0,dz):
+        q_23[j] = q_12[dx-1] + box_t * tail_shear(dy,g,i) * x_12[dx-1] * ((z_23[0])**2 - z_23[j]**2)/(4*tail_I_xx(dy,i))
+    for j in range (0,dx):
+        q_34[j] = q_23[dz-1] - tail_shear(dy,g,i) * box_t * z_23[dz-1] * x_12[j] / (2*tail_I_xx(dy,i))
+    tau_12 = np.zeros(dx)
+    tau_23 = np.zeros(dx)
+    tau_34 = np.zeros(dx)
+    for j in range (0,dx):
+        tau_12[j] = q_12[j] / box_t
+    for j in range (0,dx):
+        tau_23[j] = q_23[j] / box_t
+    for j in range (0,dx):
+        tau_34[j] = q_34[j] / box_t
+    plt.figure(figsize=(19,5))
+    plt.subplot(231)
+    plt.plot(x_12, q_12)
+    plt.ylabel('Shear, N/m')
+    plt.xlabel('Location, m')
+    plt.subplot(232)
+    plt.plot(q_23, z_23)
+    plt.ylabel('Location, m')
+    plt.xlabel('Shear, N/m')
+    plt.subplot(233)
+    plt.plot(x_34, q_34)
+    plt.ylabel('Shear, N/m')
+    plt.xlabel('Location, m')
+    plt.subplot(234)
+    plt.plot(x_12, tau_12)
+    plt.ylabel('Shear stress, N/m^2')
+    plt.xlabel('Location, m')
+    plt.subplot(235)
+    plt.plot(z_23, tau_23)
+    plt.ylabel('Location, m')
+    plt.xlabel('Shear, N/m')
+    plt.subplot(236)
+    plt.plot(x_34, tau_34)
+    plt.ylabel('Shear, N/m')
+    plt.xlabel('Location, m')
+    plt.show()   
+    return
 
 
 
