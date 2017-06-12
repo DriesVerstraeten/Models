faf98de577d0f3b58b0fd5bd9945fa441971a25b
simondm96 <simon.de.meester@telenet.be>
2017-06-12 16:37:57 +0200

    Updated classes, still WiP

--- FPP/BEM.py
+++ FPP/BEM.py
@@ -4,11 +4,15 @@
 Created on Thu Jun 08 13:46:15 2017
 
 @author: Simon
+
+This file contains the classes used for the blade element theory and propulsion
+calculations.
 """
 
 import numpy as np
+from scipy.interpolate import interp1d
 
-class blade():
+class blade(object):
     """
     Class that defines a blade
     
@@ -22,7 +26,7 @@ class blade():
         Contians the chord of the sections
         
     beta: ndarray
-        Contains the pitch of the blade sections
+        Contains the pitch (beta) of the blade sections in radians
         
     thickness: ndarray
         Contains the thickness of the sections
@@ -33,16 +37,64 @@ class blade():
         self.beta = np.array(beta)
         self.chord = np.array(chord)
         self.thickness = np.array(thickness)
-        #check is all parameters are complete
+        #check if all parameters are complete and of the same shape
         if not (len(x) == len(chord) == len(beta) == len(thickness)):
-            raise ValueError("Shape mismatch")
+            raise ValueError("Shape mismatch, make sure the inputs are of the same shape")
+            
+class airfoil(object):
+    """
+    Class that defines airfoil properties (WiP)
+    
+    It loads data from a '.npz' file with four arrays:
+        
+    airfoils: array of airfoil data arrays (AoA, cl, cd, cm)
+    thickness: array of corresponding thickness values
+    RE: Reynolds numbers for the corresponding airfoils
+    M:  Mach number for the coresponding airfoils
+    """
+    def __init__(self, filename):
+        self.filename = filename
+        self.airfoils = np.load(filename)
+        
+        #make sure the alpha values are the same everywhere        
+        alpha = []
+        airfoils = self.airfoils['airfoils']
+        for a in sorted(a for data in airfoils for a in data['alpha']):
+            if alpha and abs(a - alpha[-1]) < 1e-5:
+                continue
+            alpha.append(a)
+        self.alpha = np.array(alpha)
+        
+        lift_drag = np.dstack([
+            [np.interp(alpha, data['alpha'], data['CL']) for data in airfoils],
+            [np.interp(alpha, data['alpha'], data['CD']) for data in airfoils]
+        ])
+        self.lift_drag_by_thickness = interp1d(
+            self.aerofoils['thicknesses'], lift_drag, axis=0, copy=False)
+    
+    
 
 class BET(object):
     """
-    Class that contains the Blade Element Theory
+    Class that contains the Blade Element Theory model (WiP)
     
     Attributes
     ----------
     
+    blade: blade object
+        Contains the parameters of one blade
     
+    spinner_r: float
+        Defines the distance from the center of the disc to the root of the blade
+        
+    num_blades: int
+        Contains the amount of blades in the propellor
+        
+    airfoil: airfoil object
+        Contains the airfoil(s) polar data (AoA, cl, cd, cm)
     """
+    
+    def __init__(self, blade, spinner_r, num_blades, airfoil):
+        self.blade = blade
+        self.root_l = spinner_r
+    
