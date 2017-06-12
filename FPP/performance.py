5800af8abe2a2704e59329c1f84842f8f59528d5
simondm96 <simon.de.meester@telenet.be>
2017-06-12 14:18:29 +0200

    Added the functions in the main file

--- FPP/performance.py
+++ FPP/performance.py
@@ -4,6 +4,13 @@ Created on Fri Jun 02 17:26:23 2017
 
 @author: Simon
 
-This is the flight performance file
+This is the flight performance file where all functions come together
+
+Basically, if one needs power calculations, the propulsion file will give them.
+
+The flight performance is divided into cruise perfornmance and climb, descent 
+and field performance.
 """
 
+import propulsion as prop
+import Climb_Descent_FieldPerformance as CDFP
