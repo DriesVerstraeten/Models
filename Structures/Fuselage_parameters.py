21ae3468d304f38b02b9677970d79ef7a80f4812
claudiaraducanu <claudia.m.raducanu@gmail.com>
2017-06-14 16:50:32 +0200

    Change

--- Structures/Fuselage_parameters.py
+++ Structures/Fuselage_parameters.py
@@ -2,10 +2,9 @@
 
 ## SECTION 1: ENGINE MOUNT + FIREWALL
 a_front = 0.4  #m 
-b_front = 0.3  #m
 a_end   = 0.6  #m
-b_end   = 0.15 #m 
+b       = 0.3  #m 
 L_I     = 1.35 #m
-dl      = 0.1  #m
+dl      = 3    #m Number of Section
 ## SECTION 2: COCKPIT
 
