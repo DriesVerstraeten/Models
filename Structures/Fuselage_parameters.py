fbb84283df64096282240ab09ebd6ea705088716
claudiaraducanu <claudia.m.raducanu@gmail.com>
2017-06-14 16:37:51 +0200

    Paramters that can change for the fuselage

---
+++ Structures/Fuselage_parameters.py
@@ -0,0 +1,11 @@
+## FUSELAGE PARAMETERS
+
+## SECTION 1: ENGINE MOUNT + FIREWALL
+a_front = 0.4  #m 
+b_front = 0.3  #m
+a_end   = 0.6  #m
+b_end   = 0.15 #m 
+L_I     = 1.35 #m
+dl      = 0.1  #m
+## SECTION 2: COCKPIT
+
